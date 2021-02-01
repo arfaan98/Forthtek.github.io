be68e11a67c662de6b853bb6098292108a910576
arfaan98 <77271548+arfaan98@users.noreply.github.com>
2021-02-01 12:49:40 +0000

    !!!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Home from './pages'
+
+
+function App() {
+  return (
+    <Router>
+      <Home/>
+    </Router>
+  );
+}
+
+export default App;
